a42cdbe42818b0ce2e8f6750c3f1155a091ad8f3
Gabriele Scalici <gaba41@gmail.com>
2021-10-24 16:14:39 +0200

    world, nation, region, city queries

--- server/index.js
+++ server/index.js
@@ -17,5 +17,23 @@ app.get('/world', (req, res) => {
     const world = JSON.parse(fs.readFileSync(data));
     res.send(world);
 });
+app.get('/world/:nation', (req, res) => {
+    nation = req.params.nation
+    const place = JSON.parse(fs.readFileSync(`./data/children/${nation}/${nation}.json`));
+    res.send(place);
+})
+app.get('/world/:nation/:region', (req, res) => {
+    nation = req.params.nation
+    region = req.params.region
+    const place = JSON.parse(fs.readFileSync(`./data/children/${nation}/children/${region}/${region}.json`));
+    res.send(place);
+})
+app.get('/world/:nation/:region/:city', (req, res) => {
+    nation = req.params.nation
+    region = req.params.region
+    city = req.params.city
+    const place = JSON.parse(fs.readFileSync(`./data/children/${nation}/children/${region}/children/${city}/${city}.json`));
+    res.send(place)
+})
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
