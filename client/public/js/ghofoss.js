88681738ebc2e71fb7de0d595814436ec509d6a6
Roberto Guido <bob4job@gmail.com>
2021-10-24 17:40:49 +0200

    fetching data from client to server

--- client/public/js/ghofoss.js
+++ client/public/js/ghofoss.js
@@ -13074,6 +13074,39 @@ $('.input-daterange').datepicker({});
 $('.input-daterange input').each(function () {
   $(this).datepicker('clearDates');
 });
+
+function renderTimeline(data) {
+  var timeline = $('.timeline');
+  timeline.find('.row:not(.sample)').remove();
+  data.events.forEach(function (element) {
+    var row = timeline.find('.row.sample').first().clone();
+    row.removeClass('sample').removeClass('d-none');
+    row.find('.year').text(element.start_date.year);
+    row.find('.contents').find('.node').text(element.text.it.headline);
+    timeline.append(row);
+  });
+}
+
+function fetchData(path) {
+  $.ajax({
+    url: path,
+    method: 'GET',
+    dataType: 'JSON',
+    success: function success(data) {
+      renderTimeline(data);
+    }
+  });
+}
+
+var backend_url = 'https://ghofoss.herokuapp.com/';
+$('select[name=country]').change(function () {
+  fetchData(backend_url + $(this).find('option:selected').val());
+});
+/*
+    By default, the global timeline is fetched and rendered
+*/
+
+fetchData(backend_url + 'world');
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../node_modules/webpack/buildin/global.js */ "./node_modules/webpack/buildin/global.js")))
 
 /***/ }),
--- client/src/js/ghofoss.js
+++ client/src/js/ghofoss.js
@@ -6,3 +6,38 @@ $('.input-daterange').datepicker({});
 $('.input-daterange input').each(function() {
     $(this).datepicker('clearDates');
 });
+
+function renderTimeline(data) {
+    var timeline = $('.timeline');
+    timeline.find('.row:not(.sample)').remove();
+
+    data.events.forEach((element) => {
+        let row = timeline.find('.row.sample').first().clone();
+        row.removeClass('sample').removeClass('d-none');
+        row.find('.year').text(element.start_date.year);
+        row.find('.contents').find('.node').text(element.text.it.headline);
+        timeline.append(row);
+    });
+}
+
+function fetchData(path) {
+    $.ajax({
+        url: path,
+        method: 'GET',
+        dataType: 'JSON',
+        success: function(data) {
+            renderTimeline(data);
+        }
+    });
+}
+
+var backend_url = 'https://ghofoss.herokuapp.com/';
+
+$('select[name=country]').change(function() {
+    fetchData(backend_url + $(this).find('option:selected').val());
+});
+
+/*
+    By default, the global timeline is fetched and rendered
+*/
+fetchData(backend_url + 'world');
